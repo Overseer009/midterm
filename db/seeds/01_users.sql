32281685097cd91819552dd79f8c31278e8c555a
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-29 13:36:14 -0400

    correct the query #2

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, password) VALUES ('Mark', "mark123", "mark123");
-INSERT INTO users (username, password) VALUES ('Sacha', "sacha123", "sacha123");
-INSERT INTO users (username, password) VALUES ('Lewis', "lewis123", "lewis123");
+INSERT INTO users (username, password) VALUES ('Mark', "mark123");
+INSERT INTO users (username, password) VALUES ('Sacha', "sacha123");
+INSERT INTO users (username, password) VALUES ('Lewis', "lewis123");
 
 
 
