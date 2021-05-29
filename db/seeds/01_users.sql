8ea8b14f83d6b4d6cda15b236b82eb6875c9f882
Lewis <tinglewis633@gmail.com>
2021-05-29 14:38:12 +0000

    delete widgets seed and table, and insert dummy data to users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name,username,password) VALUES ('Mark',"mark123","mark123");
+INSERT INTO users (name,username,password) VALUES ('Sacha',"sacha123","sacha123");
+INSERT INTO users (name,username,password) VALUES ('Lewis',"lewis123","lewis123");
+
+
+
