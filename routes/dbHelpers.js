d14a395c272454224b7bd991f9d0d42550a91a11
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-29 11:38:29 -0400

    remove the widgets component from server.js and rest of files

--- routes/dbHelpers.js
+++ routes/dbHelpers.js
@@ -1,16 +1,21 @@
 
 module.exports = (db) => {
   const getUsers = () => {
-    db.query(`SELECT * FROM users;`)
+    return db.query(`SELECT * FROM users;`)
       .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
       .catch((error) => {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message })
       });
   };
-  return {getUsers}
+
+  
+
+
+
+  return {getUsers, }
 };
 
 
