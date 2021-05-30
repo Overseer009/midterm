0d5d7760245c17e6e5f952f65bf8748d2902a005
markjwhite <markwhite259@gmail.com>
2021-05-30 12:04:03 -0400

    Starting to create API endpoints

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,100 +1,109 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-//------Database Read AJAX Requests------//
+  //------Database Read AJAX Requests------//
 
   const loadUsers = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/users",
+      method: "GET",
+      url: "/api/users",
     }).done((users) => {
       for (user of users) {
-      $("<a>").text(user.username).appendTo($(".users"));
+        $("<a>").text(user.username).appendTo($(".users"));
       }
     });
   }
 
   const loadBooks = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/books",
+      method: "GET",
+      url: "/api/books",
     }).done((books) => {
       for (book of books) {
-      $("<a>").text(book.name).appendTo($(".toRead"));
+        $("<a>").text(book.name).appendTo($(".toRead"));
       }
     });
   }
 
   const loadMovies = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/movies",
+      method: "GET",
+      url: "/api/movies",
     }).done((movies) => {
       for (movie of movies) {
-      $("<a>").text(movie.name).appendTo($(".toWatch"));
+        $("<a>").text(movie.name).appendTo($(".toWatch"));
       }
     });
   }
 
   const loadRestaurants = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/restaurants",
+      method: "GET",
+      url: "/api/restaurants",
     }).done((restaurants) => {
       for (restaurant of restaurants) {
-      $("<a>").text(restaurant.name).appendTo($(".toEat"));
+        $("<a>").text(restaurant.name).appendTo($(".toEat"));
       }
     });
   }
 
   const loadProducts = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/products",
+      method: "GET",
+      url: "/api/products",
     }).done((products) => {
       for (product of products) {
-      $("<a>").text(product.name).appendTo($(".toBuy"));
+        $("<a>").text(product.name).appendTo($(".toBuy"));
       }
     });
   }
 
   const loadMisc = () => {
     $.ajax({
-        method: "GET",
-        url: "/api/misc",
+      method: "GET",
+      url: "/api/misc",
     }).done((misc) => {
       for (m of misc) {
-      $("<a>").text(m.name).appendTo($(".other"));
+        $("<a>").text(m.name).appendTo($(".other"));
       }
     });
   }
 
-//------Nav Bar Button Toggles------//
+  //------Nav Bar Button Toggles------//
 
-$(".books").click(function() {
-  $(".toRead").slideToggle("slow");
-});
+  $(".books").click(function() {
+    $(".toRead").slideToggle("slow");
+  });
 
-$(".movies").click(function() {
-  $(".toWatch").slideToggle("slow");
-});
+  $(".movies").click(function() {
+    $(".toWatch").slideToggle("slow");
+  });
 
-$(".restaurants").click(function() {
-  $(".toEat").slideToggle("slow");
-});
+  $(".restaurants").click(function() {
+    $(".toEat").slideToggle("slow");
+  });
 
-$(".products").click(function() {
-  $(".toBuy").slideToggle("slow");
-});
+  $(".products").click(function() {
+    $(".toBuy").slideToggle("slow");
+  });
 
-$(".misc").click(function() {
-  $(".other").slideToggle("slow");
-});
+  $(".misc").click(function() {
+    $(".other").slideToggle("slow");
+  });
 
-$(".dropbtn").click(function() {
-  $(".dropdown-content").slideToggle("slow");
-});
+  $(".dropbtn").click(function() {
+    $(".dropdown-content").slideToggle("slow");
+  });
 
-//------AJAX Function Calls-----//
+  //------API Routes-----//
+
+  $(".inputbutton").submit(function(event) {
+    event.preventDefault();
+    const userInput = $(".userText").val()
+    console.log(userInput);
+  })
+
+
+  //------AJAX Function Calls-----//
 
   loadUsers();
   loadBooks();
